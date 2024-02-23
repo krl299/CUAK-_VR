6f73bc916538b85a792b2b9cd65fc42059f6aac1
PeliAzuh <47522008+krl299@users.noreply.github.com>
2024-02-23 20:19:07 +0100

    project created and add file to gitignore

---
+++ Assets/Editor/CreateFolders.cs
@@ -0,0 +1,52 @@
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class CreateDefaultFolders : EditorWindow {
+
+    /// <summary>
+    /// Creates an instance of the CreateFolders window, sets its position and size, and shows it as a popup.
+    /// </summary>
+    [MenuItem("Assets/Create Default Folders")]
+    private static void SetUpFolders(){
+        CreateDefaultFolders window = GetWindow<CreateDefaultFolders>();
+        window.position = new Rect(Screen.width / 2, Screen.height / 2, 400, 150);
+        window.ShowPopup();
+    }
+
+    /// <summary>
+    /// Creates a set of predefined folders in the Unity project's Assets directory.
+    /// </summary>
+    private static void CreateAllFolders(){
+        List<string> folders = new List<string>{
+            "Animations",
+            "Audio",
+            "Editor",
+            "Materials",
+            "Models",
+            "Prefabs",
+            "Scripts",
+            "Scenes",
+            "Shaders",
+            "Textures",
+            "UI"
+        };
+
+        foreach(string folder in folders){
+            if (!Directory.Exists("Assets/" + folder)){
+                Directory.CreateDirectory("Assets/" + folder);
+            }
+        }
+
+        AssetDatabase.Refresh();
+    }
+
+    /// <summary>
+    /// Handles the GUI events for the CreateFolders window.
+    /// </summary>
+    void OnGUI(){
+        CreateAllFolders();
+        this.Close();
+    }
+}
