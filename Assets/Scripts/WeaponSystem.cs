dd1bc2b604f7ea888b85d5c7690fd83fb6283476
krl299 <kr2moran99@gmail.com>
2024-03-05 10:32:54 +0100

    created a left and right weapons on scene

--- Assets/Scripts/WeaponSystem.cs
+++ Assets/Scripts/WeaponSystem.cs
@@ -1,12 +1,22 @@
+using System;
+using Oculus.Platform;
 using OculusSampleFramework;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WeaponSystem : MonoBehaviour
 {
-    public GameObject bullet;
-    public float shootForce;
-    public Transform shootPoint;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float shootForce;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private Hand hand;
     private DistanceGrabbable _distanceGrabbable;
+    
+    enum Hand
+    {
+        Left,
+        Right
+    }
 
     private void Awake()
     {
@@ -17,9 +27,21 @@ public class WeaponSystem : MonoBehaviour
     {
         if (_distanceGrabbable.isGrabbed)
         {
-            if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+            if (hand == Hand.Left)
             {
-                Instantiate(bullet, shootPoint.position, shootPoint.rotation).GetComponent<Rigidbody>().AddForce(shootPoint.forward * shootForce);
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+                {
+                    Instantiate(bullet, shootPoint.position, shootPoint.rotation).GetComponent<Rigidbody>()
+                        .AddForce(shootPoint.forward * shootForce);
+                }
+            }
+            else if (hand == Hand.Right)
+            {
+                if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+                {
+                    Instantiate(bullet, shootPoint.position, shootPoint.rotation).GetComponent<Rigidbody>()
+                        .AddForce(shootPoint.forward * shootForce);
+                }
             }
         }
     }
